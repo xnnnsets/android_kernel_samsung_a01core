98598406ffe7c490ff32a826591beb7779cb0a7f
Almond <iwanttobebetter7@gmail.com>
2022-06-06 01:11:41 +0700

    build_kernel: little mod
    Signed-off-by: Almond

--- build_kernel.sh
+++ build_kernel.sh
@@ -9,7 +9,6 @@ export KCFLAGS=-w
 export CONFIG_SECTION_MISMATCH_WARN_ONLY=y
 
 make -C $(pwd) O=$(pwd)/out KCFLAGS=-w CONFIG_SECTION_MISMATCH_WARN_ONLY=y a01core_defconfig
-make -C $(pwd) O=$(pwd)/out KCFLAGS=-w CONFIG_SECTION_MISMATCH_WARN_ONLY=y -j16
+make -C $(pwd) O=$(pwd)/out KCFLAGS=-w CONFIG_SECTION_MISMATCH_WARN_ONLY=y -j4
 
-cp out/arch/arm/boot/Image $(pwd)/arch/arm/boot/Image
-cp out/arch/arm/boot/Image $(pwd)/arch/arm64/boot/Image
+cp out/arch/arm/boot/Image $(pwd)/out/zImage
